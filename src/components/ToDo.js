04d7c7f2a9c7e72517d1e0ed881e3eed1d629913
Lindsay Spencer <43455549+lindsayspencer@users.noreply.github.com>
2018-11-15 14:02:42 -0600

    Checkpoint React ToDo Components: completed

---
+++ src/components/ToDo.js
@@ -0,0 +1,17 @@
+// 'react' is default, { Component } is not default
+import React, { Component } from 'react';
+
+// define a subclass that extends a React Component (parent class)
+// define a render() method on the class (required)
+// the return method returns JSX - webpack parses JSX into JS during bundling - ( ) are used to avoid errors
+// JSX requirements: closing or self-closing tags; use className for adding classes to element; only one root element that contains all other elements (ex: single parent <div>)
+class ToDo extends Component {
+  render(){
+    return (
+      <li> A todo will go here </li>
+    );
+  }
+}
+// contains only a single todo item because otherwise any changes would happen to every todo item instead of individually
+// export the class to be used (imported) in another file
+export default ToDo;
